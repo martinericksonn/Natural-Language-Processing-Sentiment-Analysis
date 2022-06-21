54194c6808bfe386dbe55d7408cf56abfa923d5f
Martin <lapetajemartin@gmail.com>
2022-06-21 18:18:33 +0800

    added empty components

--- sentiment-forum/src/app/app.module.ts
+++ sentiment-forum/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { LoginComponent } from './auth-page/login/login.component';
+import { RegisterComponent } from './auth-page/register/register.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthPageComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- sentiment-forum/src/environments/environment.ts
+++ sentiment-forum/src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    apiKey: 'AIzaSyC_NYXDJcaNfP7NCju7Rw4PebBN2aXy2u8',
+
+    authDomain: 'sentiment-forum.firebaseapp.com',
+
+    projectId: 'sentiment-forum',
+
+    storageBucket: 'sentiment-forum.appspot.com',
+
+    messagingSenderId: '321336925004',
+
+    appId: '1:321336925004:web:c2177c1b7e7bb2e82f8de9',
+
+    measurementId: 'G-T8922QDQXB',
+  },
+  production: false,
 };
 
 /*
