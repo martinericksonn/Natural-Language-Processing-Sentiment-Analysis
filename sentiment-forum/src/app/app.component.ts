a92c7df6570031030d7a06c77e73c3867a6cc419
Martin <lapetajemartin@gmail.com>
2022-06-21 23:05:26 +0800

    partial sentiment algorithm

--- sentiment-forum/src/app/app.component.ts
+++ sentiment-forum/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,9 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {}
+
+  postiveWords: string[] = [];
+  negativeWords: string[] = [];
   title = 'sentiment-forum';
 
+  ngOnInit() {
+    var tempNegative: string[] = [];
+    var tempPostive: string[] = [];
+
+    this.http
+      .get('assets/positive-words.txt', {
+        responseType: 'text',
+      })
+      .subscribe((data) => {
+        console.log(data.length);
+
+        tempPostive.push(data);
+        this.postiveWords = tempPostive[0].split('\n');
+      });
+
+    this.http
+      .get('assets/negative-words.txt', {
+        responseType: 'text',
+      })
+      .subscribe((data) => {
+        console.log(data.length);
+
+        tempNegative.push(data);
+        this.negativeWords = tempNegative[0].split('\n');
+      });
+  }
+
   formText(text: string) {
-    console.log(text.split(' '));
+    var tokens = [];
+    tokens = text.split(' ');
+
+    var pos = 0;
+    var neg = 0;
+
+    var tokenPos: string[] = [];
+    var tokenNav: string[] = [];
+
+    console.log(tokens.length);
+
+    tokens.forEach((token) => {
+      if (this.postiveWords.includes(token)) {
+        tokenPos.push(token);
+        pos++;
+      } else if (this.negativeWords.includes(token)) {
+        tokenNav.push(token);
+        neg++;
+      }
+    });
+    console.log(tokenPos);
+    console.log(tokenNav);
+    console.log(pos + ' ' + neg);
   }
 }
--- sentiment-forum/src/app/app.module.ts
+++ sentiment-forum/src/app/app.module.ts
@@ -7,16 +7,12 @@ import { AuthPageComponent } from './auth-page/auth-page.component';
 import { LoginComponent } from './auth-page/login/login.component';
 import { RegisterComponent } from './auth-page/register/register.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthPageComponent,
-    LoginComponent,
-    RegisterComponent,
-  ],
+  declarations: [AuthPageComponent, LoginComponent, RegisterComponent],
 
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
